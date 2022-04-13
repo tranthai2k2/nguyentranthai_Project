c5a53fd04371e570078dc5496a2900ed8f857d8c
tranthai2k2 <97587560+tranthai2k2@users.noreply.github.com>
2022-04-13 17:09:23 +0700

    Update chuong33b.java

--- helloworld/src/helloworld/chuong33b.java
+++ helloworld/src/helloworld/chuong33b.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 class Diem{
     Scanner sc = new Scanner(System.in);
     protected  int hoanh, tung ;
+    protected int a;int b;
     protected static int x,y,z,m,n,p;
     public Diem(){
         this.hoanh =0;
@@ -12,11 +13,9 @@ class Diem{
         // int a, int b
         this.hoanh=a;
         this.tung=b;
-        System.out.print("nhap hoanh do x:");
-        a=sc.nextInt();
-        System.out.print("nhap tung do y:");
-        b=sc.nextInt();
        
+       
+        System.out.print("toa do diem : ( "+ a +" , "+ b+" )");
     }
 }
 class tamgiac extends Diem{
@@ -28,9 +27,29 @@ class tamgiac extends Diem{
        System.out.println("toa do 3 diem nhap vao A (" + x +" , " + y + ") , B(" + z +" , " + m +") , C ( " + n + " , "+ p +" )");
     }
 }
+class Duongtron  extends Diem{
+    protected  int r;
+    protected double pi=3.14;
+    protected double cv, dt;
+    public Duongtron(int a, int b,int r){
+        super(a,b);
+        this.r=r;
 
+        System.out.print("\nnhap ban kinh r: "+ r);
+        System.out.println("");
+    }
+    public double chuvi(){
+       return cv=pi*r*2;
+    }
+    public double dientich(){
+       return dt=pi*r*r;
+    }
+
+}
 public class chuong33b {
     public static void main(String[]args){
-     tamgiac a = new tamgiac();
+    Duongtron a = new Duongtron(3, 4, 5);
+    System.out.println("dien tich duong tron: "+ a.dientich());
+    System.out.println("chuvi duong tron: "+ a.chuvi());
     }
 }
