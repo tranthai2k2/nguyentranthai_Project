64974672472a533a06c145040e9a2a33cce79644
tranthai2k2 <97587560+tranthai2k2@users.noreply.github.com>
2022-04-20 18:26:56 +0700

    Update chuong33b.java

--- helloworld/src/helloworld/chuong33b.java
+++ helloworld/src/helloworld/chuong33b.java
@@ -1,30 +1,40 @@
 package helloworld;
 import java.util.Scanner;
 class Diem{
-    Scanner sc = new Scanner(System.in);
+    
     protected  int hoanh, tung ;
-    protected int a;int b;
-    protected static int x,y,z,m,n,p;
     public Diem(){
         this.hoanh =0;
         this.tung=0;
     }
-   public Diem(int a, int b){
+   public Diem(int hoanh, int tung){
+    Scanner sc = new Scanner(System.in);
         // int a, int b
-        this.hoanh=a;
-        this.tung=b;
-       
-       
-        System.out.print("toa do diem : ( "+ a +" , "+ b+" )");
+        this.hoanh=hoanh;
+        this.tung=tung;
+        System.out.println("nhập hoanh do:");
+       hoanh = sc.nextInt();
+       System.out.println("nhập tung do:");
+       tung = sc.nextInt();
+    }
+    public void InDiem(){
+        System.out.print("toa do diem : ( "+this.hoanh +" , "+ this.tung+" )");
     }
 }
+
 class tamgiac extends Diem{
-    
-   public tamgiac(){
-       Diem a1 = new Diem(x,y);
-       Diem a2 = new Diem(z, m);
-       Diem a3 = new Diem(n, p);
-       System.out.println("toa do 3 diem nhap vao A (" + x +" , " + y + ") , B(" + z +" , " + m +") , C ( " + n + " , "+ p +" )");
+    private Diem A,B,C;
+ 
+    tamgiac(){
+        A = new Diem();
+        B = new Diem();
+        C = new Diem();
+    }
+    public void Intamgiac(){
+       System.out.println("in toa do 3 diem ");
+       System.out.print("diem a: "); A.InDiem();
+       System.out.print("\ndiem b: "); B.InDiem();
+       System.out.print("\ndiem c: "); C.InDiem();
     }
 }
 class Duongtron  extends Diem{
@@ -37,19 +47,23 @@ class Duongtron  extends Diem{
 
         System.out.print("\nnhap ban kinh r: "+ r);
         System.out.println("");
+      
     }
     public double chuvi(){
-       return cv=pi*r*2;
+       return (double) pi*r*2;
     }
     public double dientich(){
-       return dt=pi*r*r;
+       return (double) pi*r*r;
     }
 
 }
 public class chuong33b {
     public static void main(String[]args){
-    Duongtron a = new Duongtron(3, 4, 5);
-    System.out.println("dien tich duong tron: "+ a.dientich());
-    System.out.println("chuvi duong tron: "+ a.chuvi());
+    // Duongtron a = new Duongtron(3, 4, 5);
+    // System.out.println("dien tich duong tron: "+ a.dientich());
+    // System.out.println("toa do tam: " ); a.InDiem();
+    // System.out.println("chuvi duong tron: "+ a.chuvi());
+    tamgiac ABC = new tamgiac();
+    ABC.Intamgiac();
     }
 }
